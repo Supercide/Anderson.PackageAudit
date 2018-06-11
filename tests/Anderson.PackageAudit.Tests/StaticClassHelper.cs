98ec1f4f8df83cf87d8c2b8a06a9302ef828c0d9
Jordan anderson <jordan_anders@live.co.uk>
2018-06-11 02:54:46 +0100

    Consume pipeline within function

---
+++ tests/Anderson.PackageAudit.Tests/StaticClassHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Anderson.PackageAudit.Tests
+{
+    public class StaticClassHelper
+    {
+        public static void Reset(Type type)
+        {
+            ConstructorInfo constructorInfo = type.TypeInitializer;
+            object[] parameters = new object[0];
+            constructorInfo.Invoke(null, parameters);
+        }
+    }
+}
