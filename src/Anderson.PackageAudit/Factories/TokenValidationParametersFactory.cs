ed2c21774cc024c76c0f50bdc7c52a6f32cba274
Jordan anderson <jordan_anders@live.co.uk>
2018-06-11 02:50:40 +0100

    Creates Token Validation factory

---
+++ src/Anderson.PackageAudit/Factories/TokenValidationParametersFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Anderson.PackageAudit.Authorization;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Anderson.PackageAudit.Factories
+{
+    public class TokenValidationParametersFactory
+    {
+        private static readonly Lazy<TokenValidationParameters> _tokenValidationParameters = new Lazy<TokenValidationParameters>(CreateTokenValidationParameters);
+
+        public static TokenValidationParameters Instance => _tokenValidationParameters.Value;
+
+        private static TokenValidationParameters CreateTokenValidationParameters()
+        {
+            var config = CreateOpenIdConnectConfiguration()
+                            .GetAwaiter()
+                            .GetResult();
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidIssuer = ConfigurationFactory.Instance["auth0:issuer"],
+                ValidAudiences = new[] { ConfigurationFactory.Instance["auth0:audience"] },
+                IssuerSigningKeys = config.SigningKeys,
+                ValidateLifetime = ConfigurationFactory.Instance["FUNCTION_ENVIRONMENT"] != WellKnownEnvironments.Test,
+            };
+            return tokenValidationParameters;
+        }
+
+        private static async Task<OpenIdConnectConfiguration> CreateOpenIdConnectConfiguration()
+        {
+            var metadataAddress = $"https://{ ConfigurationFactory.Instance["auth0:domain"]}/.well-known/openid-configuration";
+            var configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(metadataAddress, new OpenIdConnectConfigurationRetriever());
+            return await configurationManager.GetConfigurationAsync(CancellationToken.None);
+        }
+    }
+
+    public class WellKnownEnvironments
+    {
+        public const string Test = "Test";
+    }
+}
---
+++ src/Anderson.PackageAudit/Factories/WellKnownEnvironments.cs
@@ -0,0 +1,7 @@
+﻿namespace Anderson.PackageAudit.Factories
+{
+    public class WellKnownEnvironments
+    {
+        public const string Test = "Test";
+    }
+}
