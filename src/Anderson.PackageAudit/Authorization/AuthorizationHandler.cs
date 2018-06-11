49972badea62117f620a2d608550193c991324da
Jordan anderson <jordan_anders@live.co.uk>
2018-06-11 02:51:10 +0100

    Creates authorization handler

---
+++ src/Anderson.PackageAudit/Authorization/AuthorizationHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using Anderson.PackageAudit.Errors;
+using Anderson.Pipelines.Definitions;
+using Anderson.Pipelines.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Anderson.PackageAudit.Authorization
+{
+    public class AuthorizationHandler<TSuccess> : PipelineDefinition<HttpRequest, Response<TSuccess, Error>>
+    {
+        private readonly TokenValidationParameters _tokenValidationParameters;
+
+        public AuthorizationHandler(TokenValidationParameters tokenValidationParameters)
+        {
+            _tokenValidationParameters = tokenValidationParameters;
+        }
+
+        public override Response<TSuccess, Error> Handle(HttpRequest request)
+        {
+            try
+            {
+                var result = ExtractToken(request);
+                if (result.IsSuccess)
+                {
+                    Thread.CurrentPrincipal = ValidateToken(result.Success);
+                    return InnerHandler.Handle(request);
+                }
+
+                return result.Error;
+            }
+            catch (Exception e)
+            {
+                return new Error(WellKnownErrorCodes.UnAuthorized, e.Message);
+            }
+        }
+
+        private static Response<string, Error> ExtractToken(HttpRequest request)
+        {
+            if (request.Headers.ContainsKey("Authorization") ||
+                request.Headers.ContainsKey("authorization"))
+            {
+                string value = request.Headers["authorization"].First();
+                return value.Substring(7);
+            }
+
+            return new Error(WellKnownErrorCodes.UnAuthorized, "request failed authentication");
+        }
+
+        private ClaimsPrincipal ValidateToken(string token)
+        {
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.ValidateToken(
+                token,
+                _tokenValidationParameters,
+                out _);
+        }
+    }
+}
