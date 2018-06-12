993a3daa92064e3f149d6d89db0276f4d8168222
Jordan anderson <jordan_anders@live.co.uk>
2018-06-12 23:52:14 +0100

    Creates generic caching pipe

---
+++ src/Anderson.PackageAudit/SharedPipes/Caching/CachingPipe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Anderson.PackageAudit.Errors;
+using Anderson.Pipelines.Definitions;
+using Anderson.Pipelines.Responses;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+
+namespace Anderson.PackageAudit.SharedPipes.Caching
+{
+    public class CachingPipe<TRequest, TResponse> : PipelineDefinition<IList<TRequest>, Response<IList<TResponse>, Error>> 
+        where TResponse : ICachableEntity
+        where TRequest : ICachableEntity
+    {
+        private readonly IRedisTypedClient<TResponse> _redisClient;
+
+        public CachingPipe(IRedisClient redisClient)
+        {
+            _redisClient = redisClient.As<TResponse>();
+        }
+
+        public override Response<IList<TResponse>, Error> Handle(IList<TRequest> request)
+        {
+            IList<TResponse> cachedPackages = _redisClient.GetByIds(request.Select(x => x.Id));
+
+            var newPackages = request.Where(x => cachedPackages.All(package => package.Id != x.Id))
+                                     .ToList();
+
+            var response = InnerHandler.Handle(newPackages);
+
+            if (response.IsSuccess)
+            {
+                _redisClient.StoreAll(response.Success);
+
+                return response.Success
+                               .Union(cachedPackages)
+                               .ToList();
+            }
+
+            return response;
+        }
+    }
+}
---
+++ src/Anderson.PackageAudit/SharedPipes/Caching/Clients/RedisClientFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Anderson.PackageAudit.Factories;
+using ServiceStack.Redis;
+
+namespace Anderson.PackageAudit.SharedPipes.Caching.Clients
+{
+    public class RedisClientFactory
+    {
+        static Lazy<IRedisClient> _redisClient = new Lazy<IRedisClient>(() => {
+            var redisManager = new RedisManagerPool(ConfigurationFactory.Instance["redis:connectionstring"]);
+            return redisManager.GetClient();
+        });
+
+        public static IRedisClient Instance => _redisClient.Value;
+    }
+}
