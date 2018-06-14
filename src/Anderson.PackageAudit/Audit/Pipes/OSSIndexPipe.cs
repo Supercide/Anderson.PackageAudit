73c8316b7e7cbeaa4d4487083dd8d964079e7221
Jordan anderson <jordan_anders@live.co.uk>
2018-06-14 21:40:45 +0100

    Creates pipe for ossindex

---
+++ src/Anderson.PackageAudit/Audit/Pipes/OSSIndexPipe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Anderson.PackageAudit.Errors;
+using Anderson.PackageAudit.PackageModels;
+using Anderson.PackageAudit.SharedPipes.Authorization.Constants;
+using Anderson.Pipelines.Definitions;
+using Anderson.Pipelines.Responses;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Anderson.PackageAudit.Audit.Pipes
+{
+    public class OSSIndexPipe : PipelineDefinition<IList<PackageRequest>, Response<IList<Package>, Error>>
+    {
+        private readonly IConfiguration _configuration;
+        static readonly HttpClient _client = new HttpClient();
+
+        public OSSIndexPipe(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public override Response<IList<Package>, Error> Handle(IList<PackageRequest> request)
+        {
+            return HandleAsync(request).GetAwaiter().GetResult();
+        }
+
+        public async Task<Response<IList<Package>, Error>> HandleAsync(IList<PackageRequest> request)
+        {
+            if(!request.Any())
+                return new Response<IList<Package>, Error>(new List<Package>());
+
+            var requestUri = new Uri(_configuration["ossindex:uri"]);
+
+            using (var response = await _client.PostAsJsonAsync(requestUri, request.Select(x => new { pm = "nuget", name = x.Name, version = x.Version })))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    IList<Package> packages = JsonConvert.DeserializeObject<IList<Package>>(json);
+                    return new Response<IList<Package>, Error>(packages);
+                }
+
+                return new Error(WellKnownErrorCodes.OSSIndexUnavailable, "Unable to connect to sonatype");
+            }
+        }
+    }
+}
--- src/Anderson.PackageAudit/PackageModels/Package.cs
+++ src/Anderson.PackageAudit/PackageModels/Package.cs
@@ -1,9 +1,11 @@
 ﻿using Anderson.PackageAudit.SharedPipes.Caching;
+using Newtonsoft.Json;
 
 namespace Anderson.PackageAudit.PackageModels
 {
     public class Package : ICachableEntity
     {
+        [JsonIgnore]
         public string Id => $"{name}{version}".ToUpper();
         public string pm { get; set; }
         public string name { get; set; }
