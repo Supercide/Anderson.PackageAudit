fcab183afe1648eebf40394b7c351389e6abd289
Jordan anderson <jordan_anders@live.co.uk>
2018-08-05 02:51:45 +0100

    Simplyfy  error resolving

---
+++ src/Anderson.PackageAudit/Errors/ErrorResolver.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Anderson.PackageAudit.Core.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anderson.PackageAudit.Errors
+{
+
+    public class ErrorResolver : IErrorResolver
+    {
+        public IActionResult Resolve(Error error)
+        {
+            switch (error.ErrorType)
+            {
+                case ErrorType.RequestError:
+                    return new BadRequestObjectResult(error.ErrorMessage);
+                case ErrorType.AuthenticationError:
+                    return new UnauthorizedResult();
+                default:
+                    return new InternalServerErrorResult();
+            }
+        }
+    }
+}
---
+++ src/Anderson.PackageAudit/Errors/IErrorResolver.cs
@@ -0,0 +1,10 @@
+﻿using Anderson.PackageAudit.Core.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Anderson.PackageAudit.Errors
+{
+    public interface IErrorResolver
+    {
+        IActionResult Resolve(Error error);
+    }
+}
