9e9e97d8d4d4207615a59ce29da7a6cd098a7c70
Jordan anderson <jordan_anders@live.co.uk>
2018-08-05 02:53:47 +0100

    Adds extensions and wellknown types

---
+++ src/Anderson.PackageAudit/Infrastructure/PricipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Anderson.PackageAudit.Domain;
+
+namespace Anderson.PackageAudit.Infrastructure
+{
+    public static class PricipalExtensions
+    {
+        public static Account ToAccount(this IPrincipal principal)
+        {
+            var claimsPrincipal = (ClaimsPrincipal)principal;
+            var issuer = claimsPrincipal.FindFirst(x => x.Type == WellKnownOpenIdConnectClaimTypes.Issuer);
+            var authenticationId = claimsPrincipal.FindFirst(x => x.Type == WellKnownOpenIdConnectClaimTypes.NameIdentifier);
+            return new Account(issuer.Value, authenticationId.Value);
+        }
+    }
+}
---
+++ src/Anderson.PackageAudit/Infrastructure/WellKnownOpenIdConnectClaimTypes.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Anderson.PackageAudit.Infrastructure
+{
+    public class WellKnownOpenIdConnectClaimTypes
+    {
+        public const string Issuer = "iss";
+        public const string NameIdentifier = ClaimTypes.NameIdentifier;
+    }
+}
